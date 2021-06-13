f8f8feac6f9d5c8e9065e7b35897300cf583cddb
matheusmonteirotc <matheus.monteiro@tc.com.br>
2021-06-13 16:32:41 -0300

    adicionando cms prismic

--- src/pages/api/_lib/manageSubscription.ts
+++ src/pages/api/_lib/manageSubscription.ts
@@ -5,7 +5,8 @@ import { stripe } from '../../../services/stripe';
 
 export async function saveSubscription(
   subscriptionId: string,
-  customerId: string
+  customerId: string,
+  createAction: false,
 ) {
   
   const userRef = await fauna.query(
@@ -21,6 +22,37 @@ export async function saveSubscription(
 )
 
     const subscription = await stripe.subscriptions.retrieve(subscriptionId);
+
+    const subscriptionData = {
+      id: subscription.id,
+      userId: userRef,
+      status: subscription.status,
+      price_id: subscription.items.data[0].price.id,
+    }
+
+    if (createAction) {
+      await fauna.query(
+        q.Create(
+          q.Collection('subscriptions'),
+          { data: subscriptionData }
+        )
+      )
+    } else {
+      await fauna.query(
+        q.Update(
+          q.Select(
+            "ref",
+            q.Get(
+              q.Match(
+                q.Index('subscription_by_id'),
+                subscriptionId,
+              )
+            )
+          ),
+          { data: { status: subscriptionData.status } }
+        )
+      )
+    }
     
 
 }
