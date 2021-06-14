464424f5c53f56b6a15d00d0c60abfd3a75f20ac
matheustark <matheustark321@gmail.com>
2021-06-14 00:18:21 -0300

    styling menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,21 @@
 import styles from './styles.module.scss';
+import { ActiveLink } from '../ActiveLink'
+
 import { SignInButton } from '../SignInButton';
 
 export function Header() {
+
   return (
     <header className={styles.headerContainer} >
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news"/>
         <nav>
-          <a className={styles.active} >Home</a>
+          <ActiveLink activeClassName={styles.active} href='/' >
+          <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts" prefetch >
           <a>Posts</a>
+          </ActiveLink>
         </nav>
         <SignInButton />
       </div>
